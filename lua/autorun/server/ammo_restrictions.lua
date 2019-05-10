6e9665104c2b898abf66588b2328c27229cadff0
PLally <11771225+PLally@users.noreply.github.com>
2019-05-10 16:35:03 -0400

    indentation change

--- lua/autorun/server/ammo_restrictions.lua
+++ lua/autorun/server/ammo_restrictions.lua
@@ -1,13 +1,14 @@
-local blacklisted = {["gb_rocket_nebel"] = true,
-                     ["gb_shell_50mm"] = true,
-                     ["gb_shell_57mm"] = true,
-                     ["gb_shell_75mm"] = true,
-                     ["gb_shell_76mm"] = true,
-                     ["gb_shell_88mm"] = true,
-                     ["gb_shell_105mm"] = true,
-                     ["gb_shell_128mm"] = true,
-                     ["gb_shell_152mm"] = true,
-                     ["gb_shell_155mm"] = true
+local blacklisted = {
+    ["gb_rocket_nebel"] = true,
+    ["gb_shell_50mm"] = true,
+    ["gb_shell_57mm"] = true,
+    ["gb_shell_75mm"] = true,
+    ["gb_shell_76mm"] = true,
+    ["gb_shell_88mm"] = true,
+    ["gb_shell_105mm"] = true,
+    ["gb_shell_128mm"] = true,
+    ["gb_shell_152mm"] = true,
+    ["gb_shell_155mm"] = true
 }
 
 hook.Remove("OnEntityCreated", "cfc_gred_ammobox_restrictions")
