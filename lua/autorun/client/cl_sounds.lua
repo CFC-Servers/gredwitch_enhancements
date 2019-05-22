33916fcd703f59256e2abf310ef4cfd5eefa96a5
PLally <11771225+PLally@users.noreply.github.com>
2019-05-22 16:40:52 -0400

    make emplacements quieter

---
+++ lua/autorun/client/cl_sounds.lua
@@ -0,0 +1,22 @@
+local soundMultiplier = 0.7
+
+local function silenceEmplacement( ent, m )
+    if not ent.sounds then return end
+    for k, sound in pairs(ent.sounds) do
+        local soundLevel = sound:GetSoundLevel() * m
+        sound:SetSoundLevel( soundLevel * m)
+    end
+
+end
+
+local isBasedOn = scripted_ents.IsBasedOn
+
+hook.Remove("OnEntityCreated", "CFC_GredwitchEnhancementsSounds")
+hook.Add( "OnEntityCreated", "CFC_GredwitchEnhancementsSounds", function( ent )
+    local class = ent:GetClass()
+    if class == "gred_emp_base" or isBasedOn( class, "gred_emp_base" ) then
+        timer.Simple(0.1, function()
+            silenceEmplacement( ent, soundMultiplier )
+        end )
+    end
+end )
